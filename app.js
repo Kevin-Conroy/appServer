6e97c4a7445060e9a63f325285481c334db00202
Kevin Conroy <Kcdrumming@gmail.com>
2021-04-06 12:29:04 -0700

    4-6 commit, needs some review

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const morgan = require("morgan");
+
+const app = express();
+
+const cors = require('cors');
+
+app.use(morgan("common")); // let's see what 'common' format looks like
+
+app.use(cors());
+
+const apps = require("./app-data.js");
+
+
+app.get('/apps', (req, res) => {
+    const { search = "", sort } = req.query;
+  
+    if (sort) {
+      if (!['rating', 'app'].includes(sort)) {
+        return res
+          .status(400)
+          .send('Sort must be one of rating or app');
+      }
+    }
+  
+    let results = apps
+          .filter(app =>
+              app
+                .App
+                .toLowerCase()
+                .includes(search.toLowerCase()));
+  
+    if (sort) {
+      results
+        .sort((a, b) => {
+          return a[sort] > b[sort] ? 1 : a[sort] < b[sort] ? -1 : 0;
+      });
+    }
+  
+    res
+      .json(results);
+  });
